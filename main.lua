3cb069deaff906f321f9ea99f62bfb23955f89ab
GreyCaat <66911792+GreyCaat@users.noreply.github.com>
2023-04-08 23:15:36 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,26 @@
-print("hold on")
+local function SendNotification(title, text)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = title,
+        Text = text,
+        Button1 = "Ok",
+        Duration = 1.8e+308
+    })
+end
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+if syn then
+    local url
+
+    if game.PlaceId == 7230977870 then
+        url = syn.request({Url = "https://raw.githubusercontent.com/GreyCaat/Cat-Hub/main/Games/Aeronautica.lua"}).Body
+    else
+        SendNotification("Info", "Game is not supported")
+    end
+
+    if url then
+        loadstring(url)()
+    end
+else
+    SendNotification("Error", "Cat Hub only supports Synapse X")
+end
