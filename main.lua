c0cd7cff4ba711be3cb41401ad5700109acc7a6e
GreyCaat <66911792+GreyCaat@users.noreply.github.com>
2023-04-08 23:19:20 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,7 +12,10 @@ end
 if syn then
     local url
 
-    if game.PlaceId == 7230977870 then
+    if game.PlaceId == 7230977870 or game.PlaceId == 6647962258 then
+        if game.PlaceId == 6647962258 then
+            SendNotification("Info", "Join a Server")
+        end
         url = syn.request({Url = "https://raw.githubusercontent.com/GreyCaat/Cat-Hub/main/Games/Aeronautica.lua"}).Body
     else
         SendNotification("Info", "Game is not supported")
